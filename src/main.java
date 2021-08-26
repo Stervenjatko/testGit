e4941592972dc90ed85deb53fc7eeea762eff38a
stervenjatko <vedatren@gmail.com>
2021-08-26 17:52:32 +0300

    add for loop and main

--- src/main.java
+++ src/main.java
@@ -1,6 +1,9 @@
 public class  main {
     public static void main(String[] args) {
         System.out.println(" YOU BEST, maybe");
-
+        for(int i=0; i<4; i++){
+            System.out.println("or not");
+        }
+        System.out.println("but you can try");
     }
 }
